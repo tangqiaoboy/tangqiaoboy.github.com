e5924a91c04b4a00f8f8e93a9a313b85a24b21f0
Tang Qiao <tangqiao@fenbi.com>
2016-02-16 13:49:30 +0800

    ADD: add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#! /bin/zsh
+hexo clean
+hexo generate
+cd public
+git init
+git add .
+git commit -m "update at `date` "
+
+git remote add origin git@github.com:tangqiaoboy/tangqiaoboy.github.com.git >> /dev/null 2>&1
+echo "### Pushing to Github..."
+git push origin master -f
+echo "### Done"
+
+# add gitcafe source
+git remote add gitcafe git@gitcafe.com:tangqiaoboy/tangqiaoboy.git >> /dev/null 2>&1
+echo "### Pushing to GitCafe..."
+git push gitcafe master:gitcafe-pages -f
+echo "### Done"
